12a98f2233abc076b561ff9c1090a9e34b4a56a2
hmziqrs <hmziqrs@gmail.com>
2024-10-06 20:30:59 +0500

    dark mode

---
+++ app/links.ts
@@ -0,0 +1,22 @@
+export const username = 'hmziqrs';
+
+// socials links (github, x, linkedin) with icons
+
+export const links = [
+  {
+    name: 'GitHub',
+    url: `https://github.com/${username}`,
+    icon: 'github-icon',
+  },
+  {
+    name: 'X',
+    url: `https://x.com/${username}`,
+    icon: 'x-icon',
+  },
+  {
+    name: 'LinkedIn',
+    url: `https://linkedin.com/in/${username}`,
+    icon: 'linkedin-icon',
+  },
+];
+
