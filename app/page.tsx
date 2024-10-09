f5628c0da4df07a77b8b8d867e60e3a3a4483bce
hmziqrs <hmziqrs@gmail.com>
2024-10-09 13:06:19 +0500

    Fixed more responsive issue

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,26 @@ import SocialLinks from './socials';
 export default function Home() {
   return (
     <main className="fixed h-full w-full flex flex-col items-center justify-center font-[family-name:var(--font-geist-mono)]">
-      <div className="text-center sm:text-left w-full sm:max-w-4xl sm:mx-auto bg-black">
+      <div className="text-center px-0 sm:px-4 sm:text-left w-full sm:max-w-4xl sm:mx-auto">
         <h1 className="text-4xl">Busy building</h1>
         <div className="h-2" />
         <p className="text-sm">more updates coming soon!</p>
-        <div className="h-4" />
+        <div className="h-8" />
         <p className="text-lg sm:text-xl">follow @{username}</p>
-        <div className="h-4" />
+        <div className="h-2" />
         <SocialLinks />
       </div>
     </main>
   );
 }
+
+
+
+
+
+
+
+
 
 
 
